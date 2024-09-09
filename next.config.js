c0d7a22981a344544a8c592eac3ed943e0a3ae64
micposso <micposso@gmail.com>
2024-09-09 00:09:17 -0400

    fixed cloudinary not returning images

--- next.config.js
+++ next.config.js
@@ -6,7 +6,13 @@ module.exports = {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
-        pathname: "/my-account/**",
+        pathname: "/my-account/**", // This allows images from a specific Cloudinary folder
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**", // This allows images from all Cloudinary paths (if needed)
       },
     ],
   },
--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,10 +1,17 @@
 import cloudinary from "cloudinary";
 
-cloudinary.v2.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
-});
+try {
+  cloudinary.v2.config({
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+  });
+
+  console.log('Cloudinary successfully configured');
+} catch (error) {
+  console.error('Error configuring Cloudinary:', error.message);
+  // You can add additional logging services here like Sentry or LogRocket if needed
+}
 
 export default cloudinary;
